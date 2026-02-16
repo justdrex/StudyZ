ab3840c158eda59ea3cbf77c2e9476f3e0d7ba59
justdrex <1152@student.itgymnazium.cz>
2026-02-16 21:59:56 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  // ROUTES
+  if (url === "/" || url === "/home") {
+    serveHTML("index.html", res);
+  } else if (url === "/games") {
+    serveHTML("games.html", res);
+  }
+  // STATIC FILES
+  else if (url.startsWith("/assets/") || url === "/script.js" || url === "/styles.css") {
+    serveStatic(url, res);
+  } 
+  // 404
+  else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 – Nenalezeno</h1>");
+  }
+});
+
+function serveHTML(file, res) {
+  fs.readFile(path.join(__dirname, file), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Server error");
+      return;
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
+
+function serveStatic(url, res) {
+  const filePath = path.join(__dirname, url);
+  const ext = path.extname(filePath);
+
+  const mimeTypes = {
+    ".js": "application/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".css": "text/css"
+  };
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
+    res.writeHead(200, {
+      "Content-Type": mimeTypes[ext] || "application/octet-stream"
+    });
+    res.end(data);
+  });
+}
+
+server.listen(3000, () => {
+  console.log("Server běží na http://localhost:3000");
+});
